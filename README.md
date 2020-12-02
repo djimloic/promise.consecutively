0ff7b6de27be40c600c8d3c9b63dfa0b73ec1df2
Loic Djimenou <loic.djimenou@gmail.com>
2020-12-02 21:55:12 +0100

    First stable version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # promise.iterate
-When you need to chain many promises and that the order of completition matters
+
+When you need to chain many promises and that the order of completion matters.
+
+## Scenario
+
+Let's say you have an unknown list of promises that need to resolve in order. You want `promise3` to be fired only after `promise2` has successfully resolved, and `promise2` to be fired only after `promise1` has successfully resolved.
+
+When these promises are well known in advance you can chain them like this :
+
+```js
+promise1.then(() => promise2).then(() => promise3);
+```
+
+In the scenario where these promises are a dynamic list and their total number cannot be known in advance you can use `promiseIterator` to iterate over a list.
+
+```js
+promiseIterator.iterate([promise_1, promise_2, promise_3, ...., promise_n], callBackFn).then(()=>{})
+
+```
---
+++ src/promise.iterate.test.ts
@@ -0,0 +1,85 @@
+import { promiseIterator } from './promise.iterate';
+
+interface TestInterface {
+  i_attr: string;
+}
+
+class TestClass {
+  constructor(public c_attr: string) {}
+}
+
+describe('Promise Iterator', () => {
+  it('should resolve all promises for interface', () => {
+    const p1: Promise<TestInterface> = new Promise<TestInterface>(
+      (resolver, reject) => {
+        resolver({ i_attr: 'number 1' });
+      }
+    );
+    const p11: Promise<TestInterface> = p1.then((value) => ({
+      i_attr: 'number 1, one level down',
+      i_attr2: 11
+    }));
+    const p2: Promise<TestInterface> = new Promise<TestInterface>(
+      (resolver, reject) => {
+        resolver({ i_attr: 'number 2' });
+      }
+    );
+    const p3: Promise<TestInterface> = new Promise<TestInterface>(
+      (resolver, reject) => {
+        resolver({ i_attr: 'number 3' });
+      }
+    );
+
+    return promiseIterator
+      .iterate([p11, p2, p3], console.log)
+      .then((response) => {
+        // Expect All 3 promises to have been resolved
+        expect(response.length).toEqual(3);
+        // Here, promise p1, then p11 have been resolved
+        expect(response[0].i_attr).toEqual('number 1, one level down');
+        // Here, promise p2 has been resolved
+        expect(response[1].i_attr).toEqual('number 2');
+        // Here, promise p3 has been resolved
+        expect(response[2].i_attr).toEqual('number 3');
+      });
+  });
+
+  it('should failed at the first rejected promise', () => {
+    const p1: Promise<TestClass> = new Promise<TestClass>(
+      (resolver, reject) => {
+        resolver(new TestClass('number 1'));
+      }
+    );
+    const p11: Promise<TestClass> = p1.then(
+      (value) => new TestClass('number 1, one level down')
+    );
+    const p2: Promise<TestClass> = new Promise<TestClass>(
+      (resolver, reject) => {
+        reject(new TestClass('number 2'));
+      }
+    );
+    const p3: Promise<TestClass> = new Promise<TestClass>(
+      (resolver, reject) => {
+        resolver(new TestClass('number 3'));
+      }
+    );
+
+    return promiseIterator
+      .iterate([p11, p2, p3], console.log)
+      .then((response) => {
+        /**
+         * This assertion should fail the test,
+         * but this promise will not be resolve as p2 is set to fail
+         */
+        expect(1).toEqual(3);
+      })
+      .catch((err) => {
+        /**
+         * Expect the error to be p2 rejected message
+         * Note that here p1 and p11 have resolved
+         * Not that p3 will never be fired up as it is after p2 in the list
+         */
+        expect(err.c_attr).toEqual('number 2');
+      });
+  });
+});
